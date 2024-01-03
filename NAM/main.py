e7df467953a8f10c048500efe7095bc6ad626a80
systemkin <154638491+systemkin@users.noreply.github.com>
2024-01-04 00:05:21 +0300

    Add files via upload

---
+++ NAM/main.py
@@ -0,0 +1,29 @@
+from flask import *
+
+app = Flask(  # Create a flask app
+  __name__,
+  template_folder='templates',  # Name of html file folder
+  static_folder='static'  # Name of directory for static files
+)
+
+
+@app.route('/', defaults={'saved_data': None})
+@app.route('/share/<saved_data>')
+def home(saved_data):
+  if saved_data:
+    return render_template('markov.html', info = str(saved_data))
+  else:
+    return render_template('markov.html', info = "")
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('markov.html', info = "")
+
+
+
+if __name__ == "__main__":  # Makes sure this is the main process
+  app.run( # Starts the site
+    host='0.0.0.0',  # EStablishes the host, required for repl to detect the site
+    port=8800  # Randomly select the port the machine hosts on.
+  )
