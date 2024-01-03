ecaf87968581b6922093cd7f65b350dd4a236783
first_user <lexa.ms018@gmail.com>
2024-01-03 20:21:53 +0300

    Changes to be committed: 	modified:   MoneyMaker3000/assets/css/main.css 	modified:   MoneyMaker3000/assets/js/main.js 	modified:   MoneyMaker3000/index.html

--- MoneyMaker3000/assets/js/main.js
+++ MoneyMaker3000/assets/js/main.js
@@ -1,6 +1,7 @@
 function checkdata()
 {
     var label = document.getElementById("card");
+    var label2 = document.getElementById("wantedIncome");
     if (label.value === "")
     {
         document.getElementById("invalid").innerHTML = "Куда деньги отправлять?";
@@ -8,5 +9,24 @@ function checkdata()
     else if(!label.checkValidity())
         document.getElementById("invalid").innerHTML = "Неверный номер карты";
     else
-        document.getElementById("invalid").innerHTML = "Ожидайте поступления средств";
+    {
+        if((!label2.checkValidity()) || (label2.value === ""))
+            document.getElementById("invalid").innerHTML = "Неверный обьём денег";
+        else
+        {
+            document.getElementById("invalid").innerHTML = "Ожидайте поступления средств";
+            document.cookie = Date.now() + "%" + label2.value;
+        }
+    }
+}
+function income()
+{
+    if (document.cookie == "")
+        document.getElementById("income").innerHTML = "Вы ещё ничего не заработали";
+    else{
+        const words = document.cookie.split('%');
+        const time = (Date.now()-Number(words[0]))/1000/60/60/24;
+        const income = time* Number(words[1]);
+        document.getElementById("income").innerHTML = "Вы заработали " + income.toFixed(2) + " денег";
+    }
 }
